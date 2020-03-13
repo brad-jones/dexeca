ed34f14330869bc6a5578b5e22eaf9dacac801fc
Brad Jones <brad@bjc.id.au>
2020-03-13 16:26:04 +1100

    test(ping): ensure it can work cross platform

--- test/dexeca_test.dart
+++ test/dexeca_test.dart
@@ -1,11 +1,21 @@
+import 'dart:io';
 import 'package:test/test.dart';
 import 'package:dexeca/dexeca.dart';
 
 void main() {
   test('dexeca', () async {
-    var proc = await dexeca('ping', ['1.1.1.1'], inheritStdio: false);
+    var proc =
+        await dexeca('ping', _pingArgs('127.0.0.1'), inheritStdio: false);
     expect(proc.exitCode, equals(0));
     expect(proc.stderr, equals(''));
-    expect(proc.stdout, contains('1.1.1.1'));
+    expect(proc.stdout, contains('127.0.0.1'));
   });
+}
+
+List<String> _pingArgs(String target) {
+  if (Platform.isWindows) {
+    return [target];
+  } else {
+    return ['-c', '4', target];
+  }
 }
